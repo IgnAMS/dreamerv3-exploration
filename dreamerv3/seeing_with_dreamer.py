0c01df25a6e2adb33cb3b351b8e8bf9ec16cc327
IgnAMS <iams.monardes@gmail.com>
2025-12-19 12:43:23 -0300

    fix: adding parsing to config

--- dreamerv3/seeing_with_dreamer.py
+++ dreamerv3/seeing_with_dreamer.py
@@ -19,6 +19,7 @@ print("DOS\n\n")
 with open(CONFIG, "r") as f:
     cfg_dict = yaml.safe_load(f)
 config = elements.Config(cfg_dict)
+config = elements.Flags(config).parse([])
 print("TRES\n\n")
 
 task = config.task.split("_")[-1]
