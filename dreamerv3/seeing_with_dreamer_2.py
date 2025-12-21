4323a1f8d345caa50b535fdb9a158f84a79167b6
IgnAMS <iams.monardes@gmail.com>
2025-12-21 19:33:26 -0300

    fix: trying to use dyn_carry

--- dreamerv3/seeing_with_dreamer_2.py
+++ dreamerv3/seeing_with_dreamer_2.py
@@ -69,8 +69,13 @@ def reconstruct_from_prior(agent, driver, image_np, reset):
     # 1) muestrear PRIOR via imagine (policy dummy)
     policyfn = lambda feat: sample(agent.model.pol(agent.model.feat2tensor(feat), 1))
     print("carry:", jax.tree.map(describe, driver.carry))
+    _, dyn_carry, _ = driver.carry
+    rssm_state = {
+        'deter': dyn_carry['deter'][0],
+        'stoch': dyn_carry['stoch'][0],
+    }
     carry_prior, (feat_prior, action) = agent.model.dyn.imagine(
-        driver.carry[1],
+        rssm_state,
         policy=policyfn,
         length=1,
         training=False,
