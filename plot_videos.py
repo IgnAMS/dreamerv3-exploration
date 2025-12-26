8bc8f988445d0b6fb01c6c03d59f71de83c9b771
IgnAMS <iams.monardes@gmail.com>
2025-12-26 10:15:05 -0300

    refactor: changing metrichs path in plot video

--- plot_videos.py
+++ plot_videos.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import imageio.v2 as imageio
 
-METRICS_PATH = "/home/iamonardes/logdir/dreamer/cookiepedrofull18x29/size12m/02/metrics.jsonl"
+METRICS_PATH = "/home/iamonardes/logdir/dreamer/cookiepedrofull18x29/size12m/eval/metrics.jsonl"
 OUTDIR = "videos_out"
 
 os.makedirs(OUTDIR, exist_ok=True)
