4babef1975974f70ad696fd68357f01d09b15295
IgnAMS <iams.monardes@gmail.com>
2026-04-10 11:26:56 -0400

    feature: adding hergoal fixed to compare plot

--- plot_mean_runs.py
+++ plot_mean_runs.py
@@ -7,7 +7,7 @@ from pathlib import Path
 BASE = Path("~/logdir/dreamer").expanduser()
 EXPERIMENTS = {
     'POL multigoalfixed10': ('POL_multigoalfixed10/size12m', '#0022ff'),
-#     'POL HER hergoalfixed10': ('POL_HER_hergoalfixed10/size12m', '#ff4400'),
+    'POL HER hergoalfixed10': ('POL_HER_hergoalfixed10_k4/size12m', '#ff4400'),
 #     'POL multigoal9': ('POL_multigoal9/size12m', '#00aa44'),
 }
 RUNS = ['01', '02', '03', '04', '05']
